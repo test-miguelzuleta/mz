df5fe76cf5aaa984ba4f2f4332778e12af5bbd1b
test-miguelzuleta <38549861+test-miguelzuleta@users.noreply.github.com>
2019-04-23 11:05:56 -0700

    Add files via upload

---
+++ responsive/l/temps-4l/global/arrange.js
@@ -0,0 +1,13 @@
+﻿var layoutNumber = prompt('Type a number from 1 to 4 to see the different dynamically driven template layouts.\n\nYou can refresh the page and input another number to see how it changes.');
+
+var elemSrc = [
+/*#1*/ "sDMCU9pHaIc",
+/*#2*/ "screenshot-2.jpg",
+/*#3*/ "screenshot-3.jpg",
+/*#4*/ "screenshot-4.jpg"
+];
+
+var vidTheme = "dark";
+
+/*LAYOUT 4 ONLY*/
+var vidLink = "../layouts/4/video/output/compressed/test";
